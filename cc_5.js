5d13e0e395246e0dc87a8a5ccbf05b551fdf67d7
ssj2-z <ssj2@usf.edu>
2025-02-04 12:50:52 -0500

    Task 2 - Order Details.

--- cc_5.js
+++ cc_5.js
@@ -4,3 +4,17 @@ const customer = {
     age: 45,
     email: "WonderS@gmail.com"
 }
+
+// Task 2 Ordering Details
+let order = {
+    orderId: 33602,
+    totalAmount: 602,
+    status: "Processing",
+    displayOrder: function() {
+        console.log(`Order ID: ${this.orderId}`);
+        console.log(`Total Amount:
+$${this.totalAmount}`);
+            console.log(`Status: ${this.status}`);
+    }
+};
+order.displayOrder();
