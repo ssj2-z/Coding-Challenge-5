df2135da6a48e69641410575436a1ad2977a0364
ssj2-z <ssj2@usf.edu>
2025-02-05 22:29:41 -0500

    Comments and messsages edited and added.

--- cc_5.js
+++ cc_5.js
@@ -1,4 +1,5 @@
 // Task 1 Profile 
+// Customer profile
 const customer = {
     name: "Stevie Wonder",
     age: 45,
@@ -6,6 +7,7 @@ const customer = {
 }
 
 // Task 2 Ordering Details
+// Item details
 let order = {
     orderId: 33602,
     totalAmount: 602,
@@ -20,6 +22,7 @@ $${this.totalAmount}`);
 order.displayOrder();
 
 // Task 3 Arraying Tricks
+// Adding items to a database
 let cartItems = ["Basketball", "Football", "Baseball"];
 cartItems.push()
 cartItems.push("Tennisball");
@@ -29,26 +32,31 @@ cartItems.shift();
 console.log(cartItems);
 
 // Task 4 Mapping it out
+// Discounting certain pricing 
 let prices = [5, 10, 15];
 let discountedPrices = prices.map(price => price * .9);
 console.log(discountedPrices);
 
 // Task 5 Filter
+// Sorting through inventory records
 let inventory = [0, 1, 3, 4, 2];
 let availableProducts = inventory.filter(quantity => quantity > 0);
 console.log(availableProducts);
 
 // Task 6 Revenue 
+// Income cslculated
 let sales = [10, 15, 20, 30];
 let totalRevenue = sales.reduce((total, sale) => total + sale, 0);
 console.log(`Total revenue: $${totalRevenue}`);
 
 // Task 7 Finding and Sorting 
+// Sorting through records
 let customers = ["Bob", "Steve", "John", "Jordan"];
-let customer = customers.find(name => name === "John");
+let patient = customers.find(name => name === "John");
 console.log(`Customer found: ${customer}`);
 
 // Task 8 Declaration a function
+// Calculating taxes and rates
 function calculateTax(amount, taxRate) {
     const tax = amount * taxRate;
     console.log(`The calculated tax is:
@@ -57,6 +65,7 @@ $${tax.toFixed(2)}`);
 }
 
 // Task 9 Expressional Functions
+// Calculating discount
 const applyDiscount = function(price, discount) {
     const discountedPrice = price - (price * discount);
     console.log(`The discounted price is:
@@ -64,6 +73,7 @@ $${discountedPrice.toFixed(2)}`); return discountedPrice;
 };
 
 // Task 10 Arrow function
+// Calculating membership points
 const calculatePoints = (purchaseAmount) => {
     const points = Math.floor(purchaseAmount / 10);
     console.log(`You have earned ${points} points.`); 
